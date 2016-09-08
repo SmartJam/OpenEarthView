670479b0bbf423cb50b23d2d21130dfe2812c871
Clément Igonet <clement.igonet@galaxysemi.com>
2016-09-08 22:00:33 +0200

    Add C.I. files

---
+++ ci/becky.framasoft.org/ansible/playbook.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true \
+ANSIBLE_HOST_KEY_CHECKING=false \
+ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s' \
+ansible-playbook \
+--private-key=~/.ssh/id_rsa_becky \
+--user=oev \
+--connection=ssh \
+--inventory-file=./inventory \
+--limit='becky.framasoft.org' \
+./playbook.yml \
+-vvvv \
+--extra-vars "ansible_sudo_pass=$1"
